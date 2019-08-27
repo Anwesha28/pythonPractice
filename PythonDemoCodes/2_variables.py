642d6d15d9e2b4f61a40a1ea23e62880b710bbb2
Anwesha28746 <Anwesha28>
2019-08-27 22:46:00 +0530

    Initial code

---
+++ PythonDemoCodes/2_variables.py
@@ -0,0 +1,15 @@
+a=10
+b=20
+c=a+b
+print a+b
+print c
+print a*b
+a="Anwesha"
+b="Chak"
+print a
+print a+b
+'''operator overloading - Depending on the type of var + will use as addition or concartenation
+in pythin decalartion of variable not required,
+print a+b will directly print the sum of 2 values
+Print in pythin works as println in java (every print statement in pythin will print the value in the next line)
+string and int cannot be concatenated'''
